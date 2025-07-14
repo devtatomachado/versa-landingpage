e241195f0c4fc3abd0f19a73025af84d1e30576e
tato <email.tato@gmail.com>
2025-07-14 15:06:04 -0300

    update main2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, HashRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 
 const routes = createBrowserRouter([
@@ -11,13 +11,15 @@ const routes = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      { path: "/home", element: <Home /> }
+      { path: "home", element: <Home /> }
     ]
   }
-])
+], { basename: "/versa-landingpage" })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <HashRouter>
+      <RouterProvider router={routes} />
+    </HashRouter>
   </StrictMode>,
 )
